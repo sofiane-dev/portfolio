f7cf156ae9881a4c710e3a9c94c1145a3e8f6cd4
sofiane-benmahiddine <sof.benmahiddine@gmail.com>
2022-11-04 23:58:26 +0100

    fix lint errors

--- pages/works.js
+++ pages/works.js
@@ -1,17 +1,8 @@
-import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
+import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
 import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import { WorkGridItem } from '../components/grid-item'
 
-import thumbWalknote from '../public/images/works/walknote_eyecatch.png'
-import thumbFourPainters from '../public/images/works/the-four-painters_eyecatch.jpg'
-import thumbMenkiki from '../public/images/works/menkiki_eyecatch.png'
-import thumbMargelo from '../public/images/works/margelo_eyecatch.png'
-import thumbModeTokyo from '../public/images/works/modetokyo_eyecatch.png'
-import thumbStyly from '../public/images/works/styly_eyecatch.png'
-import thumbPichu2 from '../public/images/works/pichu2_eyecatch.png'
-import thumbFreeDBTagger from '../public/images/works/freedbtagger_eyecatch.png'
-import thumbAmembo from '../public/images/works/amembo_eyecatch.png'
 import thumbMirsad from '../public/images/works/mirsad.png'
 
 const Works = () => (
@@ -25,15 +16,6 @@ const Works = () => (
         <Section>
           <WorkGridItem id='mirsad' title='Mirsad' thumbnail={thumbMirsad}>
             A solution for real-time monitoring.
-          </WorkGridItem>
-        </Section>
-        <Section>
-          <WorkGridItem
-            id='walknote'
-            title='walknote'
-            thumbnail={thumbWalknote}
-          >
-            Music recommendation app for iOS
           </WorkGridItem>
         </Section>
       </SimpleGrid>
