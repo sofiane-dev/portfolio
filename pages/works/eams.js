1f02dbcf854b9695a08a9412c7766b36aa600e0a
sofiane-benmahiddine <sof.benmahiddine@gmail.com>
2022-11-09 00:07:46 +0100

    remove unused dependencies

--- pages/works/eams.js
+++ pages/works/eams.js
@@ -1,5 +1,5 @@
 import { Container, Badge, List, ListItem } from '@chakra-ui/react'
-import { Title, WorkImage, Meta } from '../../components/work'
+import { Title, Meta } from '../../components/work'
 import P from '../../components/paragraph'
 import Layout from '../../components/layouts/article'
 
